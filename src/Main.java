6071713cb09d0e1b4674a4a854555450ada5a2d7
mdrag <m.dragay@yandex.ru>
2022-12-21 20:51:29 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,13 @@ public class Main {
         var paper = 763789;
         System.out.println(paper);
 
+        dog = dog + 4.0;
+        System.out.println(dog);
+        cat = cat + 4;
+        System.out.println(cat);
+        paper = paper + 4;
+        System.out.println(paper);
+
     }
+
 }
